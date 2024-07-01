b48d1c74cf8114a3d8c0673ef920af1c5c6088b7
Shahzad Akbar <shahzadakbar369@gmail.com>
2024-07-01 15:15:00 +0500

    adding another option Fast Cash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let myBalance = 10000; //Dollar
+let myBalance = 100000; //Dollar
 let myPin = 1234;
 let pinAnswer = await inquirer.prompt([
     {
@@ -17,7 +17,7 @@ if (pinAnswer.q1 == myPin) {
             name: "operation",
             message: "What operation do you want to perform?",
             type: "list",
-            choices: ["withdraw", "Check Balance"]
+            choices: ["withdraw", "Check Balance", "Fast Cash"]
         }
     ]);
     if (operationAns.operation == "withdraw") {
@@ -36,8 +36,30 @@ if (pinAnswer.q1 == myPin) {
             console.log("Insufficient Balance!!");
         }
     }
-    if (operationAns.operation == "Check Balance") {
+    else if (operationAns.operation == "Check Balance") {
         console.log(`Your remaining balance is ${myBalance}`);
+    }
+    else if (operationAns.operation == "Fast Cash") {
+        let fastAns = await inquirer.prompt([
+            {
+                name: "fastCash",
+                message: "How much do you want to withdraw?",
+                type: "list",
+                choices: ["1000", "5000", "10000"]
+            }
+        ]);
+        if (fastAns.fastCash == "1000") {
+            myBalance -= 1000;
+            console.log(`Your new Balance is ${myBalance}`);
+        }
+        else if (fastAns.fastCash == "5000") {
+            myBalance -= 5000;
+            console.log(`Your new Balance is ${myBalance}`);
+        }
+        else if (fastAns.fastCash == "10000") {
+            myBalance -= 10000;
+            console.log(`Your new Balance is ${myBalance}`);
+        }
     }
 }
 else {
